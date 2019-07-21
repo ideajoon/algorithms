a9be1089d5a25fd178ab9084ffd7da526062fee7
IDEA JOON <49594612+ideajoon@users.noreply.github.com>
2019-07-21 23:26:04 +0900

    Update basic_part15_CircularQueues.py

--- basic_part15_CircularQueues.py
+++ basic_part15_CircularQueues.py
@@ -27,32 +27,22 @@ class CircularQueue:
     def enqueue(self, x):
         if self.isFull():
             raise IndexError('Queue full')
-        self.rear = 
-(self.rear + 1) % self.maxCount
-
+        self.rear = (self.rear + 1) % self.maxCount
         self.data[self.rear] = x
         self.count += 1
 
     def dequeue(self):
-        if 
-self.isEmpty()
-:
+        if self.isEmpty():
             raise IndexError('Queue empty')
-        self.front = 
-(self.front + 1) % self.maxCount
-
-        x = 
-self.data[self.front]
-
+        self.front = (self.front + 1) % self.maxCount
+        x = self.data[self.front]
         self.count -= 1
         return x
 
     def peek(self):
         if self.isEmpty():
             raise IndexError('Queue empty')
-        return 
-self.data[(self.front + 1) % self.maxCount]
-
+        return self.data[(self.front + 1) % self.maxCount]
 
 
 def solution(x):
