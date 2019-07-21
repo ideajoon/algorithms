362c7a43b388d0c8f7c4000d3ab48e7ff5710fc3
IDEA JOON <49594612+ideajoon@users.noreply.github.com>
2019-07-21 23:46:53 +0900

    Update basic_part23_Heaps2.py

--- basic_part23_Heaps2.py
+++ basic_part23_Heaps2.py
@@ -25,41 +25,23 @@ class MaxHeap:
 
     def maxHeapify(self, i):
         # 왼쪽 자식 (left child) 의 인덱스를 계산합니다.
-        left = 
-2 * i
-
+        left = 2 * i
         # 오른쪽 자식 (right child) 의 인덱스를 계산합니다.
-        right = 
-2 * i + 1
-
-        smallest = i
+        right = 2 * i + 1
+        greatest = i
         # 왼쪽 자식이 존재하는지, 그리고 왼쪽 자식의 (키) 값이 (무엇보다?) 더 큰지를 판단합니다.
-        if 
-left <= len(self.data) - 1
- and 
-self.data[left] > self.data[smallest]
-:
-            # 조건이 만족하는 경우, smallest 는 왼쪽 자식의 인덱스를 가집니다.
-            
-smallest = left
-
+        if left <= len(self.data) - 1 and self.data[left] > self.data[greatest]:
+            # 조건이 만족하는 경우, greatest 는 왼쪽 자식의 인덱스를 가집니다.
+            greatest = left
         # 오른쪽 자식이 존재하는지, 그리고 오른쪽 자식의 (키) 값이 (무엇보다?) 더 큰지를 판단합니다.
-        if 
-right <= len(self.data) - 1
- and 
-self.data[right] > self.data[smallest]
-:
-            # 조건이 만족하는 경우, smallest 는 오른쪽 자식의 인덱스를 가집니다.
-            
-smallest = right
-
-        if smallest != i:
+        if right <= len(self.data) - 1 and self.data[right] > self.data[greatest]:
+            # 조건이 만족하는 경우, greatest 는 오른쪽 자식의 인덱스를 가집니다.
+            greatest = right
+        if greatest != i:
             # 현재 노드 (인덱스 i) 와 최댓값 노드 (왼쪽 아니면 오른쪽 자식) 를 교체합니다.
-            
-self.data[i], self.data[smallest] = self.data[smallest], self.data[i]
-
+            self.data[i], self.data[greatest] = self.data[greatest], self.data[i]
             # 재귀적 호출을 이용하여 최대 힙의 성질을 만족할 때까지 트리를 정리합니다.
-            self.maxHeapify(smallest)
+            self.maxHeapify(greatest)
 
 
 def solution(x):
