54b67dad236ba84f334c0c10727a7b486371c31b
IDEA JOON <49594612+ideajoon@users.noreply.github.com>
2019-07-21 23:30:19 +0900

    Create basic_part17_Trees.py

---
+++ basic_part17_Trees.py
@@ -0,0 +1,11 @@
+노드 (nodes)
+간선 (edges)
+루트 노드 (root node), 리프 노드 (leaf nodes), 내부 노드 (internal nodes)
+부모 (parent) 노드와 자식 (child) 노드
+노드의 수준 (level)
+노드의 차수 (degree)
+트리의 높이 (height) - 또는, 깊이 (depth) -
+부분 트리 (서브트리; subtrees)
+이진 트리 (binary trees)
+포화 이진 트리 (full binary trees)
+완전 이진 트리 (complete binary trees)
